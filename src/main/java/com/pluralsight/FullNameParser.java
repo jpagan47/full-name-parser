6fb8869b0f75045b1d5c161d2756b10a94db329a
josepagan <josepaganrnj@outlook.com>
2026-04-14 15:55:09 -0400

    Initial Commit/ I workd on adding suffix to the name, not easy!

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
 public class FullNameParser {
+    public static void main(String[] args) {
+        String userInput ="Dana L. Wyatt, Jr";
+        //Uses \\s+ to split multiple white line spaces
+        String[] parts = userInput.split(" ");
+        System.out.println(parts[0]);
+        System.out.println(parts[1]);
+        System.out.println(parts[2]);
+        System.out.println(parts[3]);
+
+        String middleName = "(None)";
+        String lastName;
+        String suffix = "(None)";
+        if (parts.length == 4){
+            middleName = parts[1];
+            lastName = parts[2];
+            suffix = parts[3];
+        } else {
+            lastName = parts[1];
+        }
+        System.out.println("Your First Name is; " + parts[0]);
+        System.out.println("Your Middle Name is; " + middleName);
+        System.out.println("Your Last Name is; " + lastName);
+        System.out.println("Your Suffix is; " + suffix );
+
+
+    }
 }
